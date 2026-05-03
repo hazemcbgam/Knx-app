f7a8da732249475a4c3a3fda5a80fd4e7a3b85ca
hazemcbgam <hazemchgameover@gmail.com>
2026-05-03 15:45:21 +0100

    Add Kivy app displaying 'KNX PRO MAX' label

--- Main2.py
+++ Main2.py
@@ -1,1 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
 
+class TestApp(App):
+    def build(self):
+        return Label(text="KNX PRO MAX")
+
+if __name__ == "__main__":
+    TestApp().run()
